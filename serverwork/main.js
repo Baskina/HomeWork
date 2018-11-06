04e0cb657ba9d771f051cdedd6ee1cfe038ed358
kateryna.baskina <kateryna.baskina@PC1059.W3>
2018-11-06 12:21:34 +0200

    fix bag with extra loads

--- serverwork/main.js
+++ serverwork/main.js
@@ -1,42 +1,41 @@
 $(function() {
-  
-  var page;
-  var mainBase;
-  
-  renderPage(page);
-  
-  function renderPage(page) {
-    var request = new XMLHttpRequest();
-    request.open("GET", "https://reqres.in/api/users" + "?page=" + page);
-    console.log('submit');
-	request.send();	
-    
-    request.onload = function () {
-      if (this.readyState === 4 && this.status === 200) {	
-        mainBase = JSON.parse(request.responseText).data;
+	
+	var page;
+	var mainBase;
+	
+	renderPage(page);
+	
+	function renderPage(page) {
+		var request = new XMLHttpRequest();
+		request.open("GET", "https://reqres.in/api/users" + "?page=" + page);
+		console.log('submit');
+		request.send();	
+		
+		request.onload = function () {
+			if (this.readyState === 4 && this.status === 200) {	
+				mainBase = JSON.parse(request.responseText).data;
 
-        
-        mainBase.forEach( function(element, index) {
-				   makeCard(element);
-			  });
-        
-        if(page == undefined) {
-          var countLi = JSON.parse(request.responseText).total_pages;
-			console.log("here");
-			    for(var i = 0; i < countLi; i++) {
-				    $(".pagination").append("<li>" + (i + 1));
-			    }
-        }
-
-        $("li").on("click", function(event) {
-        	$(".box").empty();
-        	page = $(event.target)[0].innerHTML;
-        	console.log("start");
-        	renderPage(page);
-        });
-      }
-    }
-  }
+				
+				mainBase.forEach( function(element, index) {
+					makeCard(element);
+				});
+				
+				if(page == undefined) {
+					var countLi = JSON.parse(request.responseText).total_pages;
+					console.log("here");
+					for(var i = 0; i < countLi; i++) {
+						$(".pagination").append("<li>" + (i + 1));
+					}
+					$("li").on("click", function(event) {
+						$(".box").empty();
+						page = $(event.target)[0].innerHTML;
+						console.log("start");
+						renderPage(page);
+					});
+				}
+			}
+		}
+	}
 
 	function makeCard (user) {
 
