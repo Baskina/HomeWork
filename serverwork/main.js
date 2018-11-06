bcea8e6ef9f6ccb7ece2e6cebd7d5266d76c7cd7
kateryna.baskina <kateryna.baskina@PC1059.W3>
2018-11-06 11:58:52 +0200

    add function render page, but work with bag: 2 and more load

--- serverwork/main.js
+++ serverwork/main.js
@@ -1,53 +1,53 @@
 $(function() {
+  
+  var page;
+  var mainBase;
+  
+  renderPage(page);
+  
+  function renderPage(page) {
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://reqres.in/api/users" + "?page=" + page);
+    console.log('submit');
+	request.send();	
+    
+    request.onload = function () {
+      if (this.readyState === 4 && this.status === 200) {	
+        mainBase = JSON.parse(request.responseText).data;
 
+        
+        mainBase.forEach( function(element, index) {
+				   makeCard(element);
+			  });
+        
+        if(page == undefined) {
+          var countLi = JSON.parse(request.responseText).total_pages;
+			console.log("here");
+			    for(var i = 0; i < countLi; i++) {
+				    $(".pagination").append("<li>" + (i + 1));
+			    }
+        }
 
-
-	var request = new XMLHttpRequest();
-	var page = 1;
-
-	request.open("GET", "https://reqres.in/api/users" + "?page=" + page);
-
-
-	var mainBase;
-
-	request.onreadystatechange = function () {
-		if (request.readyState == XMLHttpRequest.DONE) {			
-			mainBase = JSON.parse(request.responseText).data;
-
-
-
-			console.log(mainBase);
-			mainBase.forEach( function(element, index) {
-				makeCard(element);
-			});
-
-
-			var countLi = JSON.parse(request.responseText).total_pages;
-			
-			for(var i = 0; i < countLi; i++) {
-				$(".pagination").append("<li>" + (i + 1));
-			}
-
-			$(".pagination li").on("click", function(event){
-				console.log($(event.target)[0].innerHTML);
-				request.open("GET", "https://reqres.in/api/users" + "?page=" + $(event.target)[0].innerHTML);
-			});
-
-		}
-	}
+        $("li").on("click", function(event) {
+        	$(".box").empty();
+        	page = $(event.target)[0].innerHTML;
+        	console.log("start");
+        	renderPage(page);
+        });
+      }
+    }
+  }
 
 	function makeCard (user) {
 
 		let card = $("<div class='card'>")
 
 		$(card)
-			.append($("<img src=" + user.avatar + ">"))
-			.append($("<div>" + user.first_name + "&nbsp;" + "</div>"))
-			.append($("<div>" + user.last_name + "</div>"));
+		.append($("<img src=" + user.avatar + ">"))
+		.append($("<div>" + user.first_name + "&nbsp;" + "</div>"))
+		.append($("<div>" + user.last_name + "</div>"));
 
 		$(".box").append(card);
 	}
-
-	request.send();
 
 });
