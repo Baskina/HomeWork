86f1b9107ce0b6e7a4e55e64cc2a815bc229a953
kateryna.baskina <kateryna.baskina@PC1059.W3>
2018-11-08 17:16:01 +0200

    add method post

--- serverwork/main.js
+++ serverwork/main.js
@@ -8,13 +8,11 @@ $(function() {
 	function renderPage(page) {
 		var request = new XMLHttpRequest();
 		request.open("GET", "https://reqres.in/api/users" + "?page=" + page);
-		console.log('submit');
 		request.send();	
 		
 		request.onload = function () {
 			if (this.readyState === 4 && this.status === 200) {	
 				mainBase = JSON.parse(request.responseText).data;
-
 				
 				mainBase.forEach( function(element, index) {
 					makeCard(element);
@@ -22,7 +20,6 @@ $(function() {
 				
 				if(page == undefined) {
 					var countLi = JSON.parse(request.responseText).total_pages;
-					console.log("here");
 					for(var i = 0; i < countLi; i++) {
 						$(".pagination").append("<li>" + (i + 1));
 					}
@@ -36,6 +33,21 @@ $(function() {
 			}
 		}
 	}
+
+	$("button[type='submit']").on('click', function() {
+		let first_name = $("input[name='first_name']").val();
+		let last_name = $("input[name='last_name']").val();
+		let avatar = $("input[name='avatar']").val();
+
+	var request = new XMLHttpRequest();
+	var param = 'first_name=' + encodeURIComponent(first_name) +
+				'last_name' + encodeURIComponent(last_name) + 
+				'avatar' + encodeURIComponent(avatar);
+	request.open('POST', "https://reqres.in/api/users");
+	request.send(param);
+	console.log(request.responseText);
+	});
+
 
 	function makeCard (user) {
 
