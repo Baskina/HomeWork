dea55b46d1742685147beb7aa53a3123ad77e324
Katya <katyabaskina@gmail.com>
2018-11-19 16:47:43 +0200

    add home-work robot

---
+++ robot/main.js
@@ -0,0 +1,113 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	let countRobots = 0;
+	let robots = {};
+	let active;
+	let stepTop;
+	let stepRight;
+	let stepBottom;
+	let stepLeft;
+
+
+	function Robot(width, height, margin, background, step, name) {
+		this.step = step;
+
+		this.elem = document.createElement('div');
+
+		this.elem.classList.add('robot-item');
+		this.elem.setAttribute('id', `robot${countRobots}`);
+
+		this.elem.style.height = height + 'px';
+		this.elem.style.width = width + 'px';
+		this.elem.style.margin = margin + 'px';
+		this.elem.style.background = background;
+		this.elem.style.position = 'relative';
+		this.elem.innerHTML = name;
+
+		if (countRobots == 1) {
+			active = this;
+			setStepCount(active);
+		}
+
+		document.getElementById('box').append(this.elem);
+
+		let btnActiveRobot = document.createElement('button');
+		btnActiveRobot.classList.add('robot-btn');
+		btnActiveRobot.setAttribute('id', `robot${countRobots}-btn`);
+		btnActiveRobot.setAttribute('for', `robot${countRobots}`);
+		btnActiveRobot.innerHTML = `robot${countRobots} is active`;
+
+		document.getElementById('active-robot').append(btnActiveRobot);
+	}
+
+	Robot.prototype.horisontal = function(domElem) {
+		let count = 0;
+
+		return function(arrow) {
+			
+			switch (arrow) {
+				case 'right': count += this.step;;
+				break;
+				case 'left': count -= this.step;;
+				break;
+			}
+			this.elem.style.left = count + 'px';
+		}.bind(domElem);
+	}
+
+	Robot.prototype.vertical = function(domElem) {
+		let count = 0;
+
+		return function(arrow) {
+			
+			switch (arrow) {
+				case 'bottom': count += this.step;
+				break;
+				case 'top': count -= this.step;
+				break;
+			}
+			this.elem.style.top = count + 'px';
+		}.bind(domElem);
+	}
+
+	function setStepCount(activeRobot) {
+		stepHorisontal = activeRobot.horisontal(activeRobot);
+		stepVertical = activeRobot.vertical(activeRobot);
+	}
+	
+	countRobots++;
+	robots.robot1 = new Robot(100, 50, 10, '#00f', 10, `robot${countRobots}`);
+	countRobots++;
+	robots.robot2 = new Robot(100, 100, 10, '#0c0f', 20, `robot${countRobots}`);
+
+	document.getElementById('add-robot').addEventListener('click', function() {
+		countRobots++;
+		robots[`robot${countRobots}`] = new Robot(50, 50, 10, '#cccccc', 5, `robot${countRobots}`);
+	});
+
+
+	document.getElementById('active-robot').addEventListener('click', function(event){
+
+		let activeRobot = event.target.getAttribute('for');
+		setStepCount(robots[activeRobot]);
+		
+	});
+
+	document.getElementById('top').addEventListener('click', function() {
+		stepVertical('top');
+	});
+	
+	document.getElementById('bottom').addEventListener('click', function() {
+		stepVertical('bottom');
+	});
+
+	document.getElementById('right').addEventListener('click', function() {
+		stepHorisontal('right');
+	});
+
+
+	document.getElementById('left').addEventListener('click', function() {
+		stepHorisontal('left');
+	});
+
+});
